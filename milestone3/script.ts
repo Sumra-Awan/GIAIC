3e15f514667353fa98f0a38e40738ac441adfc3e
Sumra-Awan <sumrajamil5@gmail.com>
2024-10-01 15:45:02 +0500

    Add files via upload

---
+++ milestone3/script.ts
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('resume-form') as HTMLFormElement;
+    const resumeDisplayElement = document.getElementById('resume-display') as HTMLDivElement;
+
+    form.addEventListener('submit', (event: Event) => {
+        event.preventDefault();
+
+        const name = (document.getElementById('name') as HTMLInputElement).value;
+        const email = (document.getElementById('email') as HTMLInputElement).value;
+        const phone = (document.getElementById('phone') as HTMLInputElement).value;
+        const education = (document.getElementById('Education') as HTMLTextAreaElement).value;
+        const experience = (document.getElementById('Experience') as HTMLTextAreaElement).value;
+        const skills = (document.getElementById('Skills') as HTMLTextAreaElement).value;
+
+        const resumeHTML = `
+        <h2><b>Resume</b></h2>
+        <h3>Personal Information</h3>
+        <p><b>Name:</b> ${name}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Phone:</b> ${phone}</p>
+        <h3>Education</h3>
+        <p>${education}</p>
+        <h3>Experience</h3>
+        <p>${experience}</p>
+        <h3>Skills</h3>
+        <p>${skills}</p>`;
+
+        if (resumeDisplayElement) {
+            resumeDisplayElement.innerHTML = resumeHTML;
+        } else {
+            console.error('The Resume Display Element Is Missing.');
+        }
+    });
+});
